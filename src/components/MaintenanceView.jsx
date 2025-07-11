e0b2857821b38ad6207963993f5e0962a2de1871
rizky-dev666 <rizkyadilald12@gmail.com>
2025-07-11 21:50:58 +0700

    maintenance mode

--- src/components/MaintenanceView.jsx
+++ src/components/MaintenanceView.jsx
@@ -1,16 +1,14 @@
+// src/components/MaintenanceView.jsx
+
 import React from "react";
-import logo from "../assets/boothstalgia.png";
 
 function MaintenanceView() {
   return (
     <div className="text-booth-cream text-center space-y-4">
-      <img
-        src={logo}
-        alt="Boothstalgia Logo"
-        className="w-24 h-auto mx-auto mb-4"
-      />
-      <h1 className="font-display text-3xl">🛠️ Situs Sedang dalam Perbaikan</h1>
-      <p className="font-title text-base leading-relaxed font-normal">
+      {/* Judul diubah dari text-3xl menjadi text-2xl */}
+      <h1 className="font-display text-2xl">🛠️ Situs Sedang dalam Perbaikan</h1>
+      {/* Deskripsi diubah dari text-base menjadi text-sm */}
+      <p className="font-title text-sm leading-relaxed font-normal">
         Kami sedang melakukan beberapa pembaruan. Kami akan segera kembali
         online!
       </p>
